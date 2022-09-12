321062c665e6096ae644bf9f8eb5dc5634340726
RPE8 <vitali.shuman96@gmail.com>
2022-09-12 17:09:54 +0300

    Set TS config and add dependescies for TS

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: './src/index.ts',
+	module: {
+		rules: [
+		  {
+			test: /\.ts?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		  },
+		  {
+			test: /\.css$/i,
+			use: ['style-loader', 'css-loader'],
+		  },
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
@@ -11,13 +27,5 @@ module.exports = {
 	devtool: 'inline-source-map',
 	devServer: {
 		static: './dist',
-	},
- 	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			}
-		],
-  	},
+	}
 };
